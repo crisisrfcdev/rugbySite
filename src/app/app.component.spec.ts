dd5f5e4be3e20209c03ba8705f36a1d5b1f2db16
theexperience <>
2020-06-15 13:09:11 -0400

    fix navlinks, add WIP message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rugbySite'`, () => {
+  it(`should have as title 'Capital Area Foundation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rugbySite');
+    expect(app.title).toEqual('Capital Area Foundation');
   });
 
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'rugbySite';
+  title = 'Capital Area Foundation';
 }
